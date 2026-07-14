591cf9908641ad951a77ec2fa654bfdac097b17d
PEDRO AKSARLIAN <49554455@ort>
2026-07-14 09:05:14 -0300

    Views y css

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,53 +13,29 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-    public IActionResult Index()
+        public IActionResult Index()
     {
         return View();
     }
 
-    public IActionResult AbrirSobre()
+   public IActionResult AbrirSobre()
     {
         List<Jugadores> sobre = BD.GenerarSobre();
-        List<FiguritaUsuario> nuevas = new List<FiguritaUsuario>();
-        foreach (Jugadores j in sobre)
-        {
-            FiguritaUsuario f = new FiguritaUsuario();
-            f.idJugador = j.ID;
-            nuevas.Add(f);
-        }
-        BD.AbrirSobre(nuevas);
-        ViewBag.Sobre = sobre;
-        return View();
-    }
-    
-     public IActionResult Pegar(int id)
-    {
-        List<FiguritaUsuario> figuritas = BD.ObtenerFiguritas();
-        FiguritaUsuario pegar = null;
-        foreach(FiguritaUsuario f in figuritas)
-        {
-            if(f.idJugador == id)
-            {
-                pegar = f;
-            }
-        }
-        if(pegar != null)
-        {
-            BD.PegarFigurita(pegar);
-        }
-        return RedirectToAction("Album");
+        BD.AbrirSobre(sobre);
+        return View(sobre);
     }
 
     public IActionResult Album()
     {
-        ViewBag.Album = BD.ObtenerFiguritasPegadas();
         ViewBag.Jugadores = BD.ObtenerJugadores();
-        return View();
+        return View(BD.ObtenerFiguritas());
     }
 
-
-
+    public IActionResult Pegar(int id)
+    {
+        BD.PegarFigurita(id);
+        return RedirectToAction("Album");
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
--- Models/BD.cs
+++ Models/BD.cs
@@ -30,43 +30,40 @@ public class BD{
         for(int i = 0; i < f.Count; i++){
             string query1 = "INSERT INTO FiguritaUsuario (Cantidad, Pegada, idJugador) VALUES (1, 0, @fidJugador)";
             string query2 = "UPDATE FiguritaUsuario SET Cantidad = Cantidad + 1 WHERE @fidJugador = idJugador";
+            FiguritaUsuario repetida = BuscarRepetida(f[i].ID);
             using(SqlConnection connection = new SqlConnection(connectionString)) 
             {
-                if(BuscarRepetida(f[i]) == null)
+                if(repetida == null)
                 {
-                    resultado = connection.Execute(query1, new {fidJugador = f[i].idJugador});
+                    resultado = connection.Execute(query1, new {fidJugador = f[i].ID});
                 }
                 else
                 {
-                    resultado = connection.Execute(query2, new {fidJugador = f[i].idJugador});
+                    resultado = connection.Execute(query2, new {fidJugador = f[i].ID});
                 }
             }
         }
         return resultado;
     }
 
-    public static FiguritaUsuario BuscarRepetida(FiguritaUsuario revisar)
+    public static FiguritaUsuario BuscarRepetida(int idJugador)
     {
         FiguritaUsuario nueva = null;
         string query = "SELECT * FROM FiguritaUsuario WHERE @fidJugador = idJugador";
         using(SqlConnection connection = new SqlConnection(connectionString))
         {
-            nueva = connection.QueryFirstOrDefault<FiguritaUsuario>(query, new {fidJugador = revisar.idJugador});
+            nueva = connection.QueryFirstOrDefault<FiguritaUsuario>(query, new {fidJugador = idJugador});
         }
         return nueva;
     }
 
-    public static int PegarFigurita(FiguritaUsuario FiguritaUsuario)
+    public static void PegarFigurita(int idJugador)
     {
-        int resultado = 0;
-        string query = "UPDATE FiguritaUsuario SET Pegada = 1 WHERE @fidJugador = idJugador";
-        if(FiguritaUsuario.Pegada == false){
-            using(SqlConnection connection = new SqlConnection(connectionString))
-            {
-                resultado = connection.Execute(query, new {fidJugador = FiguritaUsuario.idJugador});
-            }
+        string query = "UPDATE FiguritaUsuario SET Pegada = 1 WHERE idJugador = @idJugador";
+        using(SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Execute(query, new {idJugador});
         }
-        return resultado;
     }
 
     public static List<Jugadores> ObtenerJugadores()
@@ -81,16 +78,14 @@ public class BD{
 
     public static List<Jugadores> GenerarSobre()
     {
-        List<Jugadores> Jugadores = ObtenerJugadores();
+        List<Jugadores> jugadores = ObtenerJugadores();
         List<Jugadores> sobre = new List<Jugadores>();
         Random random = new Random();
-        for(int i = 0; i < 5; i++)
+        while(sobre.Count < 5)
         {
-            Jugadores jugador = null;
-            int NumRandom = random.Next(0, Jugadores.Count);
-            jugador = Jugadores[NumRandom];
-            sobre.Add(jugador);
-            Jugadores.Remove(jugador);
+            int posicion = random.Next(jugadores.Count);
+            sobre.Add(jugadores[posicion]);
+            jugadores.RemoveAt(posicion);
         }
         return sobre;
 }
